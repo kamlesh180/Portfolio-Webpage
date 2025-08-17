798f6c755a00de5b993a6d0942aca612bef33072
kamlesh <kamlesh81473@gmail.com>
2025-08-17 19:27:01 +0530

    add new code

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 var typed = new Typed(".text", {
   strings: ["Web Developer", "Frontend Developer", "Full Stack Developer"],
-  typeSpeed: 100,
+  typeSpeed: 100, 
   backSpeed: 100,
   backDelay: 1000,
   loop: true
 }); 
- 
+  
